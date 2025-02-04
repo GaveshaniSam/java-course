010bb7d0399ac770a8f0b03224deb560f67d8f6f
Gaveshani Samarawickrama <gsamarawic@wiley.com>
2025-02-04 15:55:33 +0530

    Initial Commit2

--- src/chapter2/MadLibs.java
+++ src/chapter2/MadLibs.java
@@ -1,4 +1,23 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class MadLibs {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a season");
+        String season = scanner.next();
+
+        System.out.println("Enter a whole number");
+        int number = scanner.nextInt();
+
+        System.out.println("Enter an adjective");
+        String adjective = scanner.next();
+
+        scanner.close();
+
+        System.out.println("on a "  + adjective + " " + season + " day, I drink a minimum of " + number + " cups of coffee");
+    }
 }
